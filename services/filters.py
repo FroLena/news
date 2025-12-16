df09245ddb841338e58d80b3957764eb5a9706ef
FroLena <nikita14889991@gmail.com>
2025-12-16 19:49:12 +0500

    Update filters.py

--- services/filters.py
+++ services/filters.py
@@ -1,1 +1,15 @@
+import difflib
 
+_raw_text_cache = []
+
+def is_duplicate(text):
+    global _raw_text_cache
+    for old_text in _raw_text_cache:
+        matcher = difflib.SequenceMatcher(None, text, old_text)
+        if matcher.ratio() > 0.65:
+            return True
+            
+    _raw_text_cache.append(text)
+    if len(_raw_text_cache) > 50:
+        _raw_text_cache.pop(0)
+    return False
